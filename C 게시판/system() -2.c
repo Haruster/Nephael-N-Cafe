b42735bdcee54f925dadc6f7f5b5a29311a619a8
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 05:32:53 +0900

    Create system() -2.c

---
+++ C 게시판/system() -2.c
@@ -0,0 +1,11 @@
+//system( )함수
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+ puts(" \n system 함수 \n");
+ puts(" * ipconfig (ip체크) 함수 * ");
+ system("ipconfig");
+ puts(" * ping(ping 확인) * ");
+ system("ping");
+}
