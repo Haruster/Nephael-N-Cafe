9a8486b65fa9808fe3d1c96be8bf82d501be0ee5
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-28 19:05:02 +0900

    Create system()함수.c

---
+++ C 게시판/system()함수.c
@@ -0,0 +1,11 @@
+//system( )함수
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+ puts(" \n system 함수 \n");
+ puts(" * ipconfig (ip체크) 함수 * ");
+ system("ipconfig");
+ puts(" * tree(파일 확인) * ");
+ system("tree");
+}
