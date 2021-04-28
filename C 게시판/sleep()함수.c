487b637bf439481762d505bf65b726c1c7a7d9fc
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-28 17:13:48 +0900

    Create sleep()함수.c

---
+++ C 게시판/sleep()함수.c
@@ -0,0 +1,15 @@
+//sleep( ) 문
+#include<stdio.h>
+#include<windows.h>
+void main(void)
+{
+ int x = 10;
+ int y = 20;
+ printf("program start\n");
+ Sleep(3);
+ printf("int x data [%d]\n", x);
+ Sleep(2);
+ printf("int y data [%d]\n", y);
+ Sleep(1);
+ printf("program exit\n");
+}
