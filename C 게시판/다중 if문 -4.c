8016bb229840d8231f47170fd57a3afb92ab4547
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 04:44:25 +0900

    Create 다중 if문 -4.c

---
+++ C 게시판/다중 if문 -4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(void)
+{
+    int i;
+    int x=10;
+    scanf_s("%d",&i);
+    if(i==1)
+    x=x+1;
+    else if(i==2)
+    x=x+2;
+    else
+    x=100;
+    printf("data [%d]\n",x);
+}
