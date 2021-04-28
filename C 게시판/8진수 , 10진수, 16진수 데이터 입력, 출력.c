643de1b15697ab8600185a4f32b466f09aa72971
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 05:13:47 +0900

    Create 8진수 , 10진수, 16진수 데이터 입력, 출력.c

---
+++ C 게시판/8진수 , 10진수, 16진수 데이터 입력, 출력.c
@@ -0,0 +1,11 @@
+//8진수 , 10진수, 16진수 데이터 입력, 출력
+#include<stdio.h>
+void main()
+{
+  int x=10;
+  int y=012;
+  int z=0xa;
+  printf("10진수%d\n",x);
+  printf("8진수%d\n",y);
+  printf("16진수%d\n",z);
+}
