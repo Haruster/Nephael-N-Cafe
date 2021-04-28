f684b114bb48512603a7c2d312e8bfb62f3cd447
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 05:22:59 +0900

    Create malloc() -2.c

---
+++ C 게시판/malloc() -2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int* iptr = (int*)malloc(sizeof(int));
+    char* cptr = (char*)malloc(sizeof(char));
+    double* dptr = (double*)malloc(sizeof(double));
+}
