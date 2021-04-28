abe33ea118652115e4565216ce1c1101e14803a2
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 05:20:11 +0900

    Create sprintf()함수.c

---
+++ C 게시판/sprintf()함수.c
@@ -0,0 +1,9 @@
+//sprintf()함수
+#include <stdio.h>
+int main(void)
+{
+   char  ptr[1024];
+   int   ret;
+   ret   = sprintf( ptr, "%d %x %s", 123, 123, "forum.falinux.com");
+   printf( "ret=%d ptr=%s\n", ret, ptr);
+}
