dd5a8ca3ce606eb8cdc6cd7f2e9adf8ff1c3dee8
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 04:35:39 +0900

    Create rand함수().c

---
+++ C 게시판/rand함수().c
@@ -0,0 +1,16 @@
+//rand( ) 함수
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+  int tempo[3];
+  int a;
+  srand(time(NULL));
+  for(a=1; a<=3; a++)
+  {
+    tempo[a] = rand();
+    printf("%d",tempo[a]);
+  }
+  getchar();
+}
