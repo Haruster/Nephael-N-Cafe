8a79d7d8a78d19ed429d9e993d602601fbd277d8
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 05:30:32 +0900

    Create 이중 포인트변수의 ++증가.c

---
+++ C 게시판/이중 포인트변수의 ++증가.c
@@ -0,0 +1,13 @@
+//이중 포인트변수의 ++증가
+#include<stdio.h>
+void main(void)
+{
+ int nValueArray[4] = { 10, 11, 12, };
+ int ** ptrValue = nValueArray;
+ int * ptrValue = nValueArray;
+ for (int i = 0; i<4; i++)
+ {
+  printf("Index[%d] : ptrValuee[%x] -> [%d]\n", i, ptrValue, **ptrValue, *ptrValue);
+  ptrValue++;
+ }
+}
