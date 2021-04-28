cd51d2bec7e90f114c18ed88efde2f4a67b3ede5
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-29 05:28:06 +0900

    Create sizeof -2.c

---
+++ C 게시판/sizeof -2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(void)
+{
+    printf("char data [%d]\n",sizeof(char));
+    printf("int data [%d]\n",sizeof(int));
+    printf("long data [%d]\n",sizeof(long));
+    printf("float data [%d]\n",sizeof(float));
+    printf("double data [%d]\n",sizeof(double));
+}
