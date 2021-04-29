216322576b32d22d83a3dc741de50e16dbd8c3a5
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 03:13:52 +0900

    Create c언어 GUI인터페이스 라이브러리.c

---
+++ C 게시판/c언어 GUI인터페이스 라이브러리.c
@@ -0,0 +1,8 @@
+//c언어 GUI인터페이스 라이브러리
+#include<windows.h>
+
+int CALLBACK winMain(HINSTANCE hinstance, HINSTANCE hPrevlnstance, LPSTR Ipcmd Line, int n cmdshow )
+{
+    MessageBox CNULL, "hello","helloworld",MB_OK);
+    return 0;
+}
