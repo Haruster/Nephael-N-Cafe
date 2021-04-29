881d961ec6815117b59be77b1642519296e29e38
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 03:47:56 +0900

    Create c언어 stack_example.c

---
+++ C 게시판/c언어 stack_example.c
@@ -0,0 +1,13 @@
+//c언어 stack_example.c
+#include<stdio.h>
+void test_function(int a, int b, int c, int d) {
+   int flag;
+   char buffer[10];
+
+   flag = 31337;
+   buffer[0] = 'A';
+}
+
+int main() {
+   test_function(1, 2, 3, 4);
+}
