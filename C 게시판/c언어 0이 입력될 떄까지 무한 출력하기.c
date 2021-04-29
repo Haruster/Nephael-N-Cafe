051511409b3965d640d03f4c3afc4a69755ecee6
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 05:00:08 +0900

    Create c언어 0이 입력될 떄까지 무한 출력하기.c

---
+++ C 게시판/c언어 0이 입력될 떄까지 무한 출력하기.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int a=-1;
+    {
+        while(a!=0)
+        {
+            scanf("%d", &a);
+            if(a!=0)
+            printf("%d\n",a);
+        }
+    }
+}
