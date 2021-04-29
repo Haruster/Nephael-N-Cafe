d7225b8ef8d914cf7feb6e9ebe97dae34f441186
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 04:33:24 +0900

    Create C언어 if문 응용.c

---
+++ C 게시판/C언어 if문 응용.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"stdafx.h"
+#include<string.h>
+void main()
+{
+  int i = 1;
+  int j = 2;
+  int k = 3;
+  if(i<3 | k<5)
+ 
+     printf("%d %d", i, k);
+  
+    else if(i<3 ^ k<1)
+ 
+     printf("helloworld");
+ else 
+  printf("helloworld");
+  
+}
