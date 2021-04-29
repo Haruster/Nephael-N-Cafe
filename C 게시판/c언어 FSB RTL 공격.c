31ac1716e7064b58314237f5b12a1a9cae08c9b5
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 04:32:12 +0900

    Create c언어 FSB RTL 공격.c

---
+++ C 게시판/c언어 FSB RTL 공격.c
@@ -0,0 +1,11 @@
+//c언어 FSB/RTL 공격
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char* argv[])
+{
+    char buf[20];
+    strcpy(buf, argv[1]);
+    printf(buf);
+    setuid(0);
+    system("bin/sh");
+}
