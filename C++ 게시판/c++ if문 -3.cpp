5ad94f4c13a715bf62c6c174569aa46f5e96f512
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 05:12:45 +0900

    Create c++ if문 -3.cpp

---
+++ C++ 게시판/c++ if문 -3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i;
+  cout<<"데이터를 입력하세요.";
+  cin>>i;
+  if(i<0)
+  cout<<"데이터는 음수이다.\n";
+  else if(i>0)
+  cout<<"데이터는 양수이다.\n";
+  else if(i==0)
+  cout<<"데이터는 0이다.\n";
+}
