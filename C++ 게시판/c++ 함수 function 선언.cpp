f137b8d7a1db55046136be6e236a96db65202c0b
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 05:16:43 +0900

    Create c++ 함수 function 선언.cpp

---
+++ C++ 게시판/c++ 함수 function 선언.cpp
@@ -0,0 +1,16 @@
+//c++ 함수 function 선언
+#include<iostream>
+using namespace std;
+int max(int x, int y)
+{
+  if(xx>y)
+  return x;
+  else
+  return y;
+}
+int main()
+{
+  int n;
+  n=max(2,3);
+  cout<<"연산 결과 데이터="n<<endl;
+}
