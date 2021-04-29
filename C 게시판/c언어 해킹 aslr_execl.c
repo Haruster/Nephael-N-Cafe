4c3f10c769b0e16e6c76b4018547b2a5aa0a3fb0
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 04:21:47 +0900

    Create c언어 해킹 aslr_execl.c

---
+++ C 게시판/c언어 해킹 aslr_execl.c
@@ -0,0 +1,13 @@
+//c언어 해킹 aslr_execl.c
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+   int stack_var;
+
+   // print an address from the current stack frame
+   printf("stack_var is at %p\n", &stack_var); 
+
+   // start aslr_demo, to see how it's stack is arranged
+   execl("./aslr_demo", "aslr_demo", NULL);
+}
