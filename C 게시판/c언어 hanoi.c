eac1ab4b5b2a39ba2c5ee5628749a45edc2a1a0f
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 04:29:34 +0900

    Create c언어 hanoi.c

---
+++ C 게시판/c언어 hanoi.c
@@ -0,0 +1,15 @@
+//c언어 hanoi.c
+#include<stdio.h>
+void hanoi(int n, char from, char to, char via) {
+    if(n>1) {
+        hanoi(n -1, from, via, to); {
+            printf("%c -> %c\n", from, to);
+            hanoi(n -1, from, via, to);
+        } else {
+            printf("%c -> %c\n", from, to);
+        }
+}
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    hanoi(n, 'a','
