ab60b44acd32b594ee8429ec6fec927ea9eb49f4
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 05:19:03 +0900

    Create c++ 멤버 함수 중복 정의 -2.cpp

---
+++ C++ 게시판/c++ 멤버 함수 중복 정의 -2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class printData{
+public:
+  void print(int i){cout<<i<<endl;}
+  void print(double f){cout<<f<<endl;}
+  void print(string s="no Data!"){cout<<s<<endl;}
+};
+int main(){
+  printData obj;
+  obj.print(1);
+  obj.print(3.14);
+  obj.print("c++ 14 is cool.");
+  obj.print();
+}                        
