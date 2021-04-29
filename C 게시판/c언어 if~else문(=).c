f3a64751902bc0fdd15ee63f8cd4e4f2fad4e10e
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 04:34:04 +0900

    Create c언어 if~else문(=).c

---
+++ C 게시판/c언어 if~else문(=).c
@@ -0,0 +1,21 @@
+//c언어 if~else문(=)
+#include<stdio.h>
+void main()
+{
+ int i = 1;
+ int j = 2;
+ int k = 7;
+ if( i==1 )
+ {
+  if( j==2 )
+  {
+   {
+    if(k == 3)
+     printf("i=1 j=2 k=3")
+    else 
+     printf("i=1 j=2 k=3");
+   }
+  }
+ }
+ return 0;
+}
