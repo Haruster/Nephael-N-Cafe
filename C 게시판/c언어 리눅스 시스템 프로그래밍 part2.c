b42ac51cd21e7026d6a2de5cf7ca7dcfb7aaaeb2
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-04-30 03:15:15 +0900

    Create c언어 리눅스 시스템 프로그래밍 part2.c

---
+++ C 게시판/c언어 리눅스 시스템 프로그래밍 part2.c
@@ -0,0 +1,22 @@
+// c언어 리눅스 시스템 프로그래밍 part2
+#include <stdio.h>
+
+void
+function1 ()
+{
+  printf ("Hello from function No.1\n");
+}
+
+void
+function2 ()
+{
+  printf ("Hello from function No.2\n");
+}
+
+int
+main ()
+{
+  function1();
+  function2();
+  return 0;
+}
