a1492ad91dff28469bda473bd4efffe4a0478b6d
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-06-14 03:19:37 +0900

    Create C++ 사칙연산.cpp

---
+++ C++ 게시판/C++ 사칙연산.cpp
@@ -0,0 +1,16 @@
+//c++ 데이터 사칙 연산
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 2;
+    int c = a+b;
+    int d = a-b;
+    int e = a*b;
+    int f = a/b;
+    cout<<"a+b="<<c<<endl;
+    cout<<"a-b="<<d<<endl;
+    cout<<"a*b="<<e<<endl;
+    cout<<"a/b="<<f<<endl;
+}
