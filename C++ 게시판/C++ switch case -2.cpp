8b433d8c039c4e40e6f2e18b60b4dd17c46aef81
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-06-14 03:39:01 +0900

    Create C++ switch case -2.cpp

---
+++ C++ 게시판/C++ switch case -2.cpp
@@ -0,0 +1,19 @@
+//c++ switch~case문
+#include<iostream>
+using namespace std;
+int main() 
+{
+ int i = 5;
+ switch( i )
+ {
+ case 1: 
+  cout<<"i data 1 "<<endl;
+  break;
+ case 2:
+  cout<<"i data 2 "<<endl;
+  break;
+ default:
+  cout<<"i data"<<i<<endl;
+  break;
+ }
+} 
