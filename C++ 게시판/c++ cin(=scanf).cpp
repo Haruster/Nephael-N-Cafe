9df0f7b8709178361d0aa4daf0d4256081c760b9
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-06-14 03:44:43 +0900

    Create c++ cin(=scanf).cpp

---
+++ C++ 게시판/c++ cin(=scanf).cpp
@@ -0,0 +1,11 @@
+//c++ cin(=scanf)
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number1;
+    int number2;
+    cin >> number1;
+    cin >> number2;
+    cout << " %d %d " << number1, number2;
+}
